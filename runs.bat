cc0d6cef3ddb6f694e69dd2a8567e303714ac3c8
fleckj <johannes.fleck@cancom.com>
2026-04-21 20:11:00 +0200

    add some cmd lines fror scraping

--- runs.bat
+++ runs.bat
@@ -1,3 +1,7 @@
 python .\scrape_games.py .\data\PHHFortuna05-U13-2026.json --db data/other-clubs/fortuna.db
 
 python .\scrape_games.py .\data\U15-test.json --db data/ofb_stats.db
+
+python .\scrape_games.py .\data\OstbXI-U13-2026.json --db data/ofb_stats.db
+python .\scrape_games.py .\data\OstbXI-U14-2026.json --db data/ofb_stats.db
+python .\scrape_games.py .\data\OstbXI-U15-2026.json --db data/ofb_stats.db
